d4f41b40f8568adf362174497c4665244d58aaf5
richardybanez037 <richardybanez037@gmail.com>
2025-03-31 10:06:58 +0800

    update - auth page - mobile - button width

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,19 @@ export default function Home() {
   const [showCreateAccountModal, setShowCreateAccountModal] = useState<boolean>(false)
   const [showSigninModal, setShowSigninModal] = useState<boolean>(false)
 
-  return <div className="flex flex-col sm:flex-row h-screen">
+  return <div className="flex flex-col sm:flex-row h-screen items-center">
     <div className="w-full flex justify-center items-center">
       <img src="/bird-icon.svg" className="sm:w-[11vw] w-11 mt-10" alt="like-twitter-logo" />
     </div>
-    <div className="w-full flex justify-center">
-      <div className="sm:p-12 py-12">
+    <div className="w-full flex justify-center max-w-120 px-12 sm:px-0">
+      <div className="sm:p-12 py-12 w-full">
         <h1 className="sm:text-3xl text-2xl font-bold">Current events</h1>
         <h1 className="sm:text-xl text-lg font-bold">Join today.</h1>
-        <button className={`mt-5 flex items-center justify-center 
-          w-full h-11 rounded-full gap-2 sm:px-20 px-7 bg-gray-100 
-          text-gray-900 cursor-pointer`}
+        <button className={`mt-5 flex items-center 
+          justify-center w-full h-11 
+          rounded-full gap-2 sm:px-20 
+          px-7 bg-gray-100 text-gray-900 
+          cursor-pointer`}
         >
           <img src="/google.svg" className="w-6" alt="google"/>
           <p className="sm:text-md text-sm">Sign&nbsp;up&nbsp;with&nbsp;Google</p>
@@ -40,9 +42,11 @@ export default function Home() {
         <div className="mt-15">
           <h2 className="font-bold my-4 sm:text-md text-sm">Already have an account?</h2>
           <button 
-            className={`sm:text-md text-sm flex items-center justify-center 
-              w-full h-11 rounded-full sm:px-20 px-7 bg-black border border-gray-500 
-              text-blue-500 font-bold cursor-pointer hover:bg-blue-500/10 duration-200`
+            className={`sm:text-md text-sm flex items-center
+              justify-center w-full h-11 rounded-full 
+              sm:px-20 px-7 bg-black border border-gray-500 
+              text-blue-500 font-bold cursor-pointer 
+              hover:bg-blue-500/10 duration-200`
             }
             onClick={() => setShowSigninModal(true)}
           >
